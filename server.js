b329cd91c311437320b4178ecbc604f8fe18f7fb
fahriddin <faxa99deni@gmail.com>
2024-08-21 13:33:28 +0900

    added new task train.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const res = require("express/lib/response")
 const app = express();
 const http = require("http");
 const fs = require("fs");
-
 let user;
 
 fs.readFile("database/user.json","utf8", (err, data) => {
--- train.js
+++ train.js
@@ -1,2 +1,18 @@
-console.log("train ishga tushdi")
-console.log("train ishga tushmadi")
+// A-TASK: 
+// Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+// MASALAN countLetter("e", "engineer") 3ni return qiladi.
+// @MITASK
+
+const task =(hisob,son) => {
+    let count=0;
+    for(i=0; i<=hisob.length; i++){
+        if(hisob[i] === son){
+            count++;
+        }
+    }
+    return count;
+}
+const a='assalomu alaykum'
+const b = 'a'
+const c = task(a,b)
+console.log('result:',c)
